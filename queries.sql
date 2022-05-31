680093e2ec8a24ee1f9e21a4a489232d20f105ab
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-31 20:22:15 +0500

    add select count, avg, sum, min, max, between, group by

--- queries.sql
+++ queries.sql
@@ -43,3 +43,16 @@ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 SELECT * FROM animals;
 
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals 
+	WHERE date_of_birth BETWEEN '01-JAN-1990' AND '31-DEC-2000' 
+	GROUP BY species;
