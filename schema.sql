5eae4dc7d401fbdbe1adfb489e5994e78a1a4508
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-01 21:02:07 +0500

    add create table owners

--- schema.sql
+++ schema.sql
@@ -13,3 +13,9 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 	ADD species varchar(255);
+
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY NOT NULL,
+	full_name VARCHAR(100) NOT NULL,
+	age INT NOT NULL
+);
