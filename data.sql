5f92a0e64898a6f4015f0c32a140b9dbbd3bc696
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-02 05:28:10 +0500

    insert data into owners table

--- data.sql
+++ data.sql
@@ -32,3 +32,22 @@ VALUES ('Blossom', '13-OCT-1998', 3, true, 17);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', '14-MAY-2022', 4, true, 22);
+
+INSERT INTO owners (full_name, age)
+	VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (full_name, age)
+	VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age)
+	VALUES ('Bob', 45);
+
+INSERT INTO owners (full_name, age)
+	VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (full_name, age)
+	VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (full_name, age)
+	VALUES ('Jodie Whittaker', 38);
+
