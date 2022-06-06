f2f0ca9dc222c9678fe57a00d76f682a17bd4856
Linda Keza <lindakeza19@gmail.com>
2022-06-06 17:36:27 +0200

    Insert into the table visits into data

--- data.sql
+++ data.sql
@@ -251,3 +251,10 @@ INSERT INTO visits (animals_id, vet_id,  date_of_visit)
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) 
+SELECT * FROM (SELECT id FROM animals) animal_ids, 
+(SELECT id FROM vets) vets_ids, 
+generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
