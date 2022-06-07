0a2a7f4f810a09e56fabe3176e3a51e979573818
Linda Keza <lindakeza19@gmail.com>
2022-06-07 16:50:01 +0200

    Create table for treatments and invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,19 @@ CREATE TABLE medical_histories(
 	status VARCHAR(255),
 	PRIMARY KEY(id)
 );
+
+CREATE TABLE treatments(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	type VARCHAR(255),
+	name VARCHAR(255),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	total_amount DECIMAL,
+	generated_at DATE,
+	payed_at DATE,
+	medical_history_id INT,
+	PRIMARY KEY(id)
+);
