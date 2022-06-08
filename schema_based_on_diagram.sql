8ce9fa0e570b4a75abdf1863838864bd355d85dc
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-08 08:00:00 +0500

    update index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -61,7 +61,7 @@ CREATE TABLE histories_treatment(
 );
 
 CREATE INDEX invoices_medical_history_id_idx ON invoices(medical_history_id);
-CREATE INDEX invoice_items_invoice_id_idx ON invoice_items(invoice_id);
+CREATE INDEX invoice_items_invoice_id_idx ON invoice_items(invoices_id);
 CREATE INDEX invoice_items_treatment_id_idx ON invoice_items(treatment_id);
 CREATE INDEX medical_histories_patient_id_idx ON medical_histories(patient_id);
 
